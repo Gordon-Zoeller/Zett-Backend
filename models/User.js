7ee9f820549a6157977a4df6c9e28d63e92ca7c3
anonymous <email>
2023-12-16 11:03:08 +0100

    create: user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true, index: {unique: true}},
+    password: {type: String, required: true},
+    role: {type: String, enum: ["admin", "user"], default: user},
+    address: {
+        street: String,
+        zip: String,
+        city: String,
+        country: String
+    },
+    orders: [{type: Schema.Types.ObjectId, ref: "order"}],
+    reviews: [{type: Schema.Types.ObjectId, ref: "reviews"}]
+});
+
+const UserModel = model("User", UserSchema);
+
+export default UserModel;
