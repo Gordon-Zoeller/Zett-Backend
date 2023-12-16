b6d1c8d32e282d63b52fe26460a3fd00a79234af
anonymous <email>
2023-12-16 11:33:50 +0100

    update: quotation marks around ref user; thumbnail

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new Schema({
     lastName: {type: String, required: true},
     email: {type: String, required: true, index: {unique: true}},
     password: {type: String, required: true},
-    role: {type: String, enum: ["admin", "user"], default: user},
+    thumbnail: String,
+    role: {type: String, enum: ["admin", "user"], default: "user"},
     address: {
         street: String,
         zip: String,
