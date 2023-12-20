c2048da7dd6afebeb59f65e406fef3ef6531f7d4
anonymous <email>
2023-12-20 11:03:55 +0100

    update: added token to register

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,13 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import UserModel from "../models/User.js";
 
 const register = async (req, res, next) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = await UserModel.create({...req.body, password: hashedPassword});
-        res.json({success: true, data: user});
+        const token = jwt.sign({_id: user._id, email: user.email}, process.env.TOKEN_KEY, {issuer: "Zett"});
+        res.header("token", token).json({success: true, data: user});
     } catch (error) {
         next(error);
     };
