7940680df7b2b9ae24af1fa9c958edc88064888d
anonymous <email>
2023-12-23 09:48:27 +0100

    create: BookModel

---
+++ models/Book.js
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+
+const BookSchema = new Schema({
+    title: {type: String, required: true},
+    author: {
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true}
+    },
+    year: {type: Number, required: true},
+    publisher: {type: String, required: true},
+    genre: String,
+    price: {type: Number, required: true},
+    ISBN: {type: String, required: true},
+    reviews: [{type: Schema.Types.ObjectId, ref: "Reviews"}],
+    thumbnail: String
+});
+
+const BookModel = model("Book", BookSchema);
+
+export default BookModel;
