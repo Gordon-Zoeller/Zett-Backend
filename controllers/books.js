2172f2f61126573345ae4b7114331fa6052b64af
anonymous <email>
2023-12-29 11:21:06 +0100

    update: createBook controller: changed data to message in res; added fileName and data variables to collect req; use data variable to create book

--- controllers/books.js
+++ controllers/books.js
@@ -2,8 +2,25 @@ import BookModel from "../models/Book.js";
 
 const createBook = async (req, res, next) => {
     try {
-        const book = await BookModel.create(req.body);
-        res.json({success: true, data: book});
+        const fileName = Date.now() + "_" + req.files.image.name;
+        const data = {
+            title: req.body.title,
+            author: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName
+            },
+            year: parseInt(req.body.year),
+            publisher: req.body.publisher,
+            price: parseFloat(req.body.price),
+            ISBN: req.body.isbn,
+            image: {
+                fileName: fileName,
+                data: req.files.image.data,
+                thumbnail: `${process.env.THUMBNAIL}${fileName}`
+            }
+        };
+        const book = await BookModel.create(data);
+        res.json({success: true, message: "The book was uploaded successfully."});
     } catch (error) {
         next(error);
     };
