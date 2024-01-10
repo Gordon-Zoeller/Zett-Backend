7b5032447db4c6b8edeb08d8cfa755ecfc99f7b1
anonymous <email>
2024-01-10 15:21:17 +0100

    create: genre route

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createBook } from "../controllers/books.js";
+import { createBook, genre } from "../controllers/books.js";
 import { authorization } from "../middleware/authorization.js";
 import { role } from "../middleware/role.js";
 
 const routes = Router();
 
 routes.post("/upload", authorization, role, createBook);
+routes.get("/genre/:genre", genre);
 
 export default routes;
