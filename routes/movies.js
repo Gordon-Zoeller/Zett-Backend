1b55eb27a45914bbf9ecac298ca10d5134e86077
anonymous <email>
2024-02-15 14:22:43 +0100

    create: genre route; image route

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createMovie } from "../controllers/movies.js";
+import { createMovie, genre, image } from "../controllers/movies.js";
 import { authorization } from "../middleware/authorization.js";
 import { role } from "../middleware/role.js";
 
 const routes = Router();
 
 routes.post("/upload", authorization, role, createMovie);
+routes.get("/genre/:genre", genre);
+routes.get("/image/:fileName", image);
 
 export default routes;
