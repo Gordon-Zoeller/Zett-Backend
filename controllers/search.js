aa733e99283660b08d4a5616848a6d51d08b7b9d
anonymous <email>
2024-03-06 15:01:06 +0100

    update: searching movies

--- controllers/search.js
+++ controllers/search.js
@@ -1,19 +1,22 @@
 import BookModel from "../models/Book.js";
+import MovieModel from "../models/Movie.js";
 
 const search = async (req, res, next) => {
     try {
         const space = req.params.query.replaceAll("+", " ");
-        let book;
+        let product;
         switch(req.params.category) {
             case "books": {
-                book = await BookModel.find({$or: [{title: {$regex: space, $options: "i"}}, {author: {$regex: space, $options: "i"}}]});
+                product = await BookModel.find({$or: [{title: {$regex: space, $options: "i"}}, {author: {$regex: space, $options: "i"}}]});
             };
             break;
-            case "movies": {};
+            case "movies": {
+                product = await MovieModel.find({$or: [{title: {$regex: space, $options: "i"}}, {director: {$regex: space, $options: "i"}}, {actors: {$regex: space, $options: "i"}}]});
+            };
             break;
             default: {};
         };
-        res.json({success: true, data: book});
+        res.json({success: true, data: product});
     } catch (error) {
         next(error);
     }
