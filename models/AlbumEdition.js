89eaf83022a5dac4a7e66ea98b2ccde37800c4bb
anonymous <email>
2024-03-07 15:15:39 +0100

    update: added index to imn

--- models/AlbumEdition.js
+++ models/AlbumEdition.js
@@ -3,6 +3,7 @@ import { Schema } from "mongoose";
 export const AlbumEditionSchema = new Schema({
     year: {type: Number, required: true},
     price: {type: Number, required: true},
+    imn: {type: String, required: true, index: {unique: true}},
     image: {
         fileName: {type: String, required: true},
         data: {type: Buffer},
