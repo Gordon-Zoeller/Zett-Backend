f8a1469e69d90f2d39378652e14a4f2d537b9d29
anonymous <email>
2024-03-07 10:57:06 +0100

    update: added imn

--- models/Album.js
+++ models/Album.js
@@ -5,6 +5,7 @@ const AlbumSchema = new Schema({
     title: {type: String, required: true},
     artist: {type: String, required: true},
     label: {type: String, required: true},
+    imn: {type: Number, required: true},
     genre: {type: String, required: true},
     runTime: {type: String, required: true},
     tracks: [String],
