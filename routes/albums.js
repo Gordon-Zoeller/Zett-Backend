913249bc967c4b57bd957be62f7c719cb6366e06
anonymous <email>
2024-03-08 11:24:48 +0100

    create: genre route

--- routes/albums.js
+++ routes/albums.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createAlbum } from "../controllers/albums.js";
+import { createAlbum, genre } from "../controllers/albums.js";
 import { authorization } from "../middleware/authorization.js";
 import { role } from "../middleware/role.js";
 
 const routes = Router();
 
 routes.post("/upload", authorization, role, createAlbum);
+routes.get("/genre/:genre", genre);
 
 export default routes;
