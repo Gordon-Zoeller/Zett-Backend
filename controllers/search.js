a19297d26ccdb47263e386b61f0b8b66630bf6f2
anonymous <email>
2024-03-11 11:15:29 +0100

    update: added search for albums

--- controllers/search.js
+++ controllers/search.js
@@ -1,20 +1,18 @@
+import { emptySpace } from "../helpers/emptySpace.js";
+import AlbumModel from "../models/Album.js";
 import BookModel from "../models/Book.js";
 import MovieModel from "../models/Movie.js";
 
 const search = async (req, res, next) => {
     try {
-        const space = req.params.query.replaceAll("+", " ");
+        const space = emptySpace(req.params.query);
         let product;
         switch(req.params.category) {
-            case "books": {
-                product = await BookModel.find({$or: [{title: {$regex: space, $options: "i"}}, {author: {$regex: space, $options: "i"}}]});
-            };
-            break;
-            case "movies": {
-                product = await MovieModel.find({$or: [{title: {$regex: space, $options: "i"}}, {director: {$regex: space, $options: "i"}}, {actors: {$regex: space, $options: "i"}}]});
-            };
-            break;
-            default: {};
+            case "books": product = await BookModel.find({$or: [{title: {$regex: space, $options: "i"}}, {author: {$regex: space, $options: "i"}}]});
+                break;
+            case "movies": product = await MovieModel.find({$or: [{title: {$regex: space, $options: "i"}}, {director: {$regex: space, $options: "i"}}, {actors: {$regex: space, $options: "i"}}]});
+                break;
+            default: product = await AlbumModel.find({$or: [{title: {$regex: space, $options: "i"}}, {artist: {$regex: space, $options: "i"}}]});
         };
         res.json({success: true, data: product});
     } catch (error) {
